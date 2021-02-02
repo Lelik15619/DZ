f664a98b48efcd7ff6a8d51b14f45c20111b49ab
Lelik15619 <71852056+Lelik15619@users.noreply.github.com>
2021-02-02 14:34:13 +0300

    Delete Lesson2_2 directory

--- Lesson2_2/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package Lesson2_2;
-
-public class MyArrayDataException extends NumberFormatException{
-    public MyArrayDataException(int i, int j) {
-        super("Неверные данные в массиве строка " + (i + 1) + " в столбец " + (j + 1) );
-    }
-}
--- Lesson2_2/MyArraySizeException.java
+++
@@ -1,8 +0,0 @@
-package Lesson2_2;
-
-public class MyArraySizeException extends ArrayIndexOutOfBoundsException{
-
-    public MyArraySizeException() {
-        super("Размер массива неверен!");
-    }
-}
